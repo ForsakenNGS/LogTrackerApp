fb353c14caa90581fdc8a2be620c6d6e79c10337
Jens N <jens.n@godlike.biz>
2023-03-18 16:24:47 +0100

    Added first lua import and update loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod updater;
 
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Builder, FileChooserButton, Entry};
+use gtk::{prelude::*, MessageDialog, Window, DialogFlags, MessageType, ButtonsType};
+use gtk::{Application, ApplicationWindow, Builder, FileChooserButton, Entry, Statusbar};
 use updater::Updater;
 use std::sync::{Arc, Mutex};
 use std::path::Path;
+use std::thread;
 
 fn build_gui(application: &Application) {
     let updater_arc = Arc::new(Mutex::new(Updater::new()));
@@ -20,19 +21,35 @@ fn build_gui(application: &Application) {
     let api_key_entry_updater = updater_arc.clone();
     let api_secret_entry: Entry = builder.object::<Entry>("api_secret").unwrap();
     let api_secret_entry_updater = updater_arc.clone();
+    let status: Statusbar = builder.object::<Statusbar>("status").unwrap();
     // Initialize saved values
-    let mut updater = updater_arc.lock().unwrap();
-    updater.load_config();
-    let game_dir_str = updater.get_game_dir();
-    file_picker.set_current_folder(Path::new(game_dir_str));
-    api_key_entry.set_text(updater.get_api_key());
-    api_secret_entry.set_text(updater.get_api_secret());
+    {
+        let mut updater = updater_arc.lock().unwrap();
+        updater.load_config();
+        updater.read_addon_data();
+        let game_dir_str = updater.get_game_dir();
+        file_picker.set_current_folder(Path::new(game_dir_str));
+        api_key_entry.set_text(updater.get_api_key());
+        api_secret_entry.set_text(updater.get_api_secret());
+        let (update_pos, update_max) = updater.update_next();
+        println!("Updated {} / {}", update_pos, update_max);
+    }
     // Connect relevant signals
     file_picker.connect_file_set(move |file_picker: &FileChooserButton| {
         let game_dir = file_picker.filename().expect("No directory chosen!");
         let game_dir_str = game_dir.to_str().unwrap().clone();
-        file_picker_updater.lock().unwrap().set_game_dir(&game_dir_str);
-        println!("Game dir picked! {}", game_dir_str);
+        let mut game_dir_wtf = game_dir.clone();
+        game_dir_wtf.push("WTF");
+        if game_dir_wtf.is_dir() {
+            file_picker_updater.lock().unwrap().set_game_dir(&game_dir_str);
+        } else {
+            let error_dialog = MessageDialog::new(
+                None::<&Window>, DialogFlags::empty(), MessageType::Error, ButtonsType::Close, 
+                "Invalid game directory!"
+            );
+            error_dialog.run();
+            error_dialog.close();
+        }
     });
     api_key_entry.connect_changed(move |api_key_entry: &Entry| {
         let api_key = api_key_entry.text();
@@ -46,6 +63,19 @@ fn build_gui(application: &Application) {
     });
     window.set_application(Some(application));
     window.show_all();
+    // Start updater thread
+    let updater_thread = updater_arc.clone();
+    let _bg_thread = thread::spawn(move || {
+        loop {
+            let (update_pos, update_max) = updater_thread.lock().unwrap().update_next();
+            println!("Updated {} / {}", update_pos, update_max);
+            /*
+            let context_id = status.context_id("Update progress");
+            let status_text = format!("Updated {} / {}", update_pos, update_max);
+            status.push(context_id, status_text.as_str());
+            */
+        }
+    });
 }
 
 fn main() {
--- src/updater.rs
+++ src/updater.rs
@@ -1,10 +1,17 @@
 extern crate serde;
 extern crate serde_json;
 
-use serde::{Serialize, Deserialize};
 use std::env::var;
+use std::path::PathBuf;
 use std::fs::{self, File};
-use std::io::{Read, Write, Result};
+use std::io::Write;
+use mlua::prelude::*;
+use mlua::Table;
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+use graphql_client::{GraphQLQuery, Response};
+use std::time::{Duration, SystemTime};
+use std::thread::sleep;
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct UpdaterConfig {
@@ -13,14 +20,29 @@ pub struct UpdaterConfig {
     api_secret: Box<str>
 }
 
+#[derive(Clone)]
+pub struct UpdaterPlayer {
+    realm: Box<str>,
+    name: Box<str>,
+    pending: bool,
+    last_update: u64,
+    last_update_logs: u64
+}
+
 pub struct Updater {
-    config: UpdaterConfig
+    config: UpdaterConfig,
+    players: HashMap<String, HashMap<String, UpdaterPlayer>>,
+    update_queue: Vec<UpdaterPlayer>,
+    update_queue_pos: usize
 }
 
 impl Updater {
     pub fn new() -> Updater {
         Updater{
-            config: Default::default()
+            config: Default::default(),
+            players: HashMap::new(),
+            update_queue: Vec::new(),
+            update_queue_pos: 0
         }
     }
 
@@ -36,6 +58,16 @@ impl Updater {
         &self.config.api_secret
     }
 
+    pub fn get_player(&mut self, realm: &String, player_name: &String) -> &mut UpdaterPlayer {
+        let realm_players = self.players.entry(realm.clone()).or_insert_with(|| HashMap::new());
+        realm_players.entry(player_name.clone()).or_insert_with(|| {
+            UpdaterPlayer{
+                realm: realm.as_str().into(), name: player_name.as_str().into(), 
+                pending: false, last_update: 0, last_update_logs: 0
+            }
+        })
+    }
+
     pub fn set_game_dir(&mut self, game_dir: &str) {
         self.config.game_dir = game_dir.into();
         self.write_config();
@@ -49,6 +81,74 @@ impl Updater {
     pub fn set_api_secret(&mut self, api_secret: &str) {
         self.config.api_secret = api_secret.into();
         self.write_config();
+    }
+
+    pub fn read_addon_data(&mut self) {
+        let game_dir_str = String::from(self.config.game_dir.clone());
+        let game_dir = PathBuf::from(game_dir_str);
+        let mut game_wtf_accounts = PathBuf::from(game_dir.clone());
+        game_wtf_accounts.push("WTF");
+        game_wtf_accounts.push("Account");
+        if !game_wtf_accounts.is_dir() {
+            return
+        }
+        for account_dir in game_wtf_accounts.read_dir().expect("Failed to read WoW-Accounts!") {
+            if let Ok(account_dir) = account_dir {
+                let mut addon_lua_saved = PathBuf::from(account_dir.path());
+                addon_lua_saved.push("SavedVariables");
+                addon_lua_saved.push("LogTracker.lua");
+                if addon_lua_saved.is_file() {
+                    let lua = Lua::new();
+                    let data_raw = fs::read_to_string(addon_lua_saved).unwrap();
+                    if !lua.load(data_raw.as_str()).exec().is_err() {
+                        let data: Table = lua.globals().get("LogTrackerDB").unwrap();
+                        let data_realms: Table = data.get("playerData").unwrap();
+                        for pair_realm in data_realms.pairs::<String, Table>() {
+                            let (realm_name, player_list) = pair_realm.unwrap();
+                            for pair_player in player_list.pairs::<String, Table>() {
+                                let (player_name, player_details) = pair_player.unwrap();
+                                let player_updated: u64 = player_details.get("lastUpdate").unwrap();
+                                let player_updated_logs: u64 = player_details.get("lastUpdateLogs").or_else(|_| Ok::<u64, u64>(0)).unwrap();
+                                let mut player = &mut self.get_player(&realm_name, &player_name);
+                                player.last_update = player_updated;
+                                player.last_update_logs = player_updated_logs;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        let mut addon_lua_import = PathBuf::from(game_dir);
+        addon_lua_import.push("Interface");
+        addon_lua_import.push("AddOns");
+        addon_lua_import.push("LogTracker");
+        addon_lua_import.push("AppData.lua");
+        if addon_lua_import.is_file() {
+            let lua = Lua::new();
+            let data_raw = fs::read_to_string(addon_lua_import).unwrap();
+            if !lua.load(data_raw.as_str()).exec().is_err() {
+                // TODO: Load previously exported entries
+            }
+        }
+        self.rewrite_update_queue();
+    }
+
+    pub fn rewrite_update_queue(&mut self) {
+        self.update_queue.clear();
+        for pair_realm in self.players.iter() {
+            let (_realm_name, player_list) = pair_realm;
+            for pair_player in player_list.iter() {
+                let (_player_name, player_details) = pair_player;
+                self.update_queue.push(player_details.clone());
+            }
+        }
+        self.update_queue.sort_by(|a, b| {
+            if a.last_update_logs == b.last_update_logs {
+                b.last_update.cmp(&a.last_update)
+            } else {
+                a.last_update_logs.cmp(&b.last_update_logs)
+            }
+        });
     }
 
     pub fn load_config(&mut self) {
@@ -70,6 +170,21 @@ impl Updater {
         let data = serde_json::to_string(&self.config);
         file.write_all(data.unwrap().as_bytes())
             .expect("Failed to write configuration");
+    }
+
+    pub fn update_next(&mut self) -> (usize,usize) {
+        let update_index = self.update_queue_pos;
+        let update_count = self.update_queue.len();
+        if update_index >= update_count {
+            sleep(Duration::new(1, 0));
+            return (update_index, update_count);
+        }
+        self.update_queue_pos += 1;
+        let player = self.update_queue.get_mut(update_index).unwrap();
+        player.last_update_logs = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+        //let client = UnionQuery::build_query();
+        // TODO: Query updated logs
+        return (update_index+1, update_count);
     }
 
 }
