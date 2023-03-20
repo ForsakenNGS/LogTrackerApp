15e1f85e86d48fbb3c38e9155ba9b75305913883
Jens N <jens.n@godlike.biz>
2023-03-20 20:43:35 +0100

    Changed GUI from gtk to egui

--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,149 @@
 mod updater;
 
-use gtk::{prelude::*, MessageDialog, Window, DialogFlags, MessageType, ButtonsType, glib};
-use gtk::{Application, ApplicationWindow, Builder, FileChooserButton, Entry, Statusbar};
+use eframe::egui;
+use tinyfiledialogs::MessageBoxIcon;
 use std::time::{SystemTime, Duration};
 use updater::Updater;
 use std::sync::{Arc, Mutex};
-use std::path::Path;
-use std::thread;
-enum Message {
-    UpdateStatus(String),
+use std::path::PathBuf;
+use std::thread::{self, JoinHandle};
+
+struct LogTrackerApp {
+    game_dir: String,
+    api_id: String,
+    api_secret: String,
+    status_text: Arc<Mutex<String>>,
+    updater_arc: Arc<Mutex<Updater>>,
+    updater_thread: Option<JoinHandle<()>>
 }
 
-fn build_gui(application: &Application) {
-    let updater_arc = Arc::new(Mutex::new(Updater::new()));
-    // Initialize GUI
-    let glade_file = include_str!("../gui/log_tracker_app.glade");
-    let builder = Builder::from_string(glade_file);
-    // Get relevant GUI-Elements
-    let window: ApplicationWindow = builder.object::<ApplicationWindow>("main_window")
-        .expect("Unable to initialize main window!");
-    let file_picker: FileChooserButton = builder.object::<FileChooserButton>("game_dir_picker").unwrap();
-    let file_picker_updater = updater_arc.clone();
-    let api_key_entry: Entry = builder.object::<Entry>("api_key").unwrap();
-    let api_key_entry_updater = updater_arc.clone();
-    let api_secret_entry: Entry = builder.object::<Entry>("api_secret").unwrap();
-    let api_secret_entry_updater = updater_arc.clone();
-    let status: Statusbar = builder.object::<Statusbar>("status").unwrap();
-    let window_delete_updater = updater_arc.clone();
-    // Initialize saved values
-    {
-        let mut updater = updater_arc.lock().unwrap();
+impl LogTrackerApp {
+
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        let mut updater = Updater::new();
         updater.load_config();
         updater.read_addon_data();
-        let game_dir_str = updater.get_game_dir();
-        file_picker.set_current_folder(Path::new(game_dir_str));
-        api_key_entry.set_text(updater.get_api_key());
-        api_secret_entry.set_text(updater.get_api_secret());
+        Self{
+            game_dir: updater.get_game_dir().to_owned(),
+            api_id: updater.get_api_key().to_owned(),
+            api_secret: updater.get_api_secret().to_owned(),
+            status_text: Arc::new(Mutex::new("".to_owned())),
+            updater_arc: Arc::new(Mutex::new(updater)),
+            updater_thread: None
+        }
     }
-    // Connect relevant signals
-    file_picker.connect_file_set(move |file_picker: &FileChooserButton| {
-        let game_dir = file_picker.filename().expect("No directory chosen!");
-        let game_dir_str = game_dir.to_str().unwrap().clone();
-        let mut game_dir_wtf = game_dir.clone();
-        game_dir_wtf.push("WTF");
-        if game_dir_wtf.is_dir() {
-            file_picker_updater.lock().unwrap().set_game_dir(&game_dir_str);
-        } else {
-            let error_dialog = MessageDialog::new(
-                None::<&Window>, DialogFlags::empty(), MessageType::Error, ButtonsType::Close, 
-                "Invalid game directory!"
-            );
-            error_dialog.run();
-            error_dialog.close();
-        }
-    });
-    api_key_entry.connect_changed(move |api_key_entry: &Entry| {
-        let api_key = api_key_entry.text();
-        let api_key_str = api_key.as_str();
-        api_key_entry_updater.lock().unwrap().set_api_key(&api_key_str);
-    });
-    api_secret_entry.connect_changed(move |api_key_entry: &Entry| {
-        let api_key = api_key_entry.text();
-        let api_key_str = api_key.as_str();
-        api_secret_entry_updater.lock().unwrap().set_api_secret(&api_key_str);
-    });
-    window.connect_delete_event(move |_, _| {
-        window_delete_updater.lock().unwrap().stop();
-        Inhibit(false)
-    });
-    window.set_application(Some(application));
-    window.show_all();
-    // Start updater thread
-    let (sender, receiver) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let updater_thread = updater_arc.clone();
-    let status_thread = Arc::new(Mutex::new(status));
-    let _bg_thread = thread::spawn(move || {
-        let mut last_status_update = SystemTime::now();
-        let mut last_export = SystemTime::now();
-        let mut pause_until = SystemTime::now();
-        loop {
-            if !updater_thread.lock().unwrap().is_active() {
-                updater_thread.lock().unwrap().write_addon_data();
-                break;
+
+    pub fn start_update_thread(&mut self) {
+        let updater_thread = self.updater_arc.clone();
+        let status_text_thread  = self.status_text.clone();
+        self.updater_thread = Some(thread::spawn(move || {
+            let mut last_status_update = SystemTime::now();
+            let mut last_export = SystemTime::now();
+            let mut pause_until = SystemTime::now();
+            loop {
+                if !updater_thread.lock().unwrap().is_active() {
+                    updater_thread.lock().unwrap().write_addon_data();
+                    break;
+                }
+                if pause_until > SystemTime::now() {
+                    continue;
+                }
+                updater_thread.lock().unwrap().update_addon();
+                let (update_pos, update_max, pause) = updater_thread.lock().unwrap().update_next();
+                let last_status_update_secs = SystemTime::now().duration_since(last_status_update).unwrap().as_secs();
+                if last_status_update_secs > 2 {
+                    let status_text = format!("Updated {} / {}", update_pos, update_max);
+                    *status_text_thread.lock().unwrap() = status_text;
+                    last_status_update = SystemTime::now();
+                }
+                let last_export_secs = SystemTime::now().duration_since(last_export).unwrap().as_secs();
+                if last_export_secs > 30 {
+                    updater_thread.lock().unwrap().write_addon_data();
+                    last_export = SystemTime::now();
+                }
+                if pause {
+                    pause_until = SystemTime::now() + Duration::new(300, 0);
+                }
             }
-            if pause_until > SystemTime::now() {
-                continue;
-            }
-            updater_thread.lock().unwrap().update_addon();
-            let (update_pos, update_max, pause) = updater_thread.lock().unwrap().update_next();
-            let last_status_update_secs = SystemTime::now().duration_since(last_status_update).unwrap().as_secs();
-            if last_status_update_secs > 2 {
-                let status_text = format!("Updated {} / {}", update_pos, update_max);
-                let _result = sender.send(Message::UpdateStatus(status_text));
-                last_status_update = SystemTime::now();
-            }
-            let last_export_secs = SystemTime::now().duration_since(last_export).unwrap().as_secs();
-            if last_export_secs > 30 {
-                updater_thread.lock().unwrap().write_addon_data();
-                last_export = SystemTime::now();
-            }
-            if pause {
-                pause_until = SystemTime::now() + Duration::new(300, 0);
-            }
-        }
-    });
-    receiver.attach(None, move |msg| {
-        match msg {
-            Message::UpdateStatus(text) => {
-                let context_id = status_thread.lock().unwrap().context_id("Update progress");
-                status_thread.lock().unwrap().push(context_id, text.as_str());
-            }
-        }
-        glib::Continue(true)
-    });
+        }));
+    }
+
 }
 
-fn main() {
+impl eframe::App for LogTrackerApp {
+
+    fn on_close_event(&mut self) -> bool {
+        self.updater_arc.lock().unwrap().stop();
+        if let Some(updater_thread) = self.updater_thread.take() {
+            updater_thread.join().unwrap();
+        }
+        true
+    }
+
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.set_min_width(300.0);
+            ui.set_min_height(200.0);
+            ui.vertical(|ui| {
+                ui.label("Game directory");
+                ui.horizontal(|ui| {
+                    ui.add( 
+                        egui::TextEdit::singleline(&mut self.game_dir)
+                            .desired_width(ui.available_width() - 100.0).interactive(false)
+                    );
+                    //ui.input(reader)
+                    if ui.button("Choose...").clicked() {
+                        if let Some(path) = tinyfiledialogs::select_folder_dialog("Game directory", self.game_dir.as_str()) {
+                            let mut game_dir_wtf = PathBuf::from(path.clone());
+                            game_dir_wtf.push("WTF");
+                            if game_dir_wtf.is_dir() {
+                                let mut updater = self.updater_arc.lock().unwrap();
+                                updater.set_game_dir(&path);
+                            } else {
+                                tinyfiledialogs::message_box_ok("Error", "Invalid game directory!", MessageBoxIcon::Error);
+                            }
+                        }
+                    }
+                });
+                let label_api_id = ui.label("WCL API Client-ID");
+                let input_api_id = ui.add(
+                    egui::TextEdit::singleline(&mut self.api_id)
+                        .desired_width(ui.available_width())
+                ).labelled_by(label_api_id.id);
+                if input_api_id.changed() {
+                    let mut updater = self.updater_arc.lock().unwrap();
+                    updater.set_api_key(&self.api_id);
+                }
+                let label_api_secret = ui.label("WCL API Client-Secret");
+                let input_api_secret = ui.add(
+                    egui::TextEdit::singleline(&mut self.api_secret)
+                        .desired_width(ui.available_width()).password(true)
+                ).labelled_by(label_api_secret.id);
+                if input_api_secret.changed() {
+                    let mut updater = self.updater_arc.lock().unwrap();
+                    updater.set_api_secret(&self.api_secret);
+                }
+                ui.set_height( ui.available_height() - 15.0 );
+            });
+            ui.label(self.status_text.lock().unwrap().to_owned());
+        });
+    }
+}
+
+fn main() -> Result<(), eframe::Error> {
     env_logger::init();
-    let app = Application::builder()
-        .application_id("org.forsaken.LogTracker")
-        .build();
-
-    app.connect_activate(build_gui);
-
-    app.run();
+    // Initialize GUI
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(400.0, 300.0)),
+        min_window_size: Some(egui::vec2(350.0, 250.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "LogTracker App", options,
+        Box::new(|cc| {
+            let mut app = LogTrackerApp::new(cc);
+            app.start_update_thread();
+            Box::new(app)
+        })
+    )
 }
